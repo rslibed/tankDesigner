6fa0560cf3c94b23a690d138a466ffd556998f75
rslibed <rslibed@gmail.com>
2018-01-02 11:32:53 -0800

    Added mobility to tank, changes direction of movement based on boundary, rotating turret and fires cannons

--- includes/tanks/ryansTank.js
+++ includes/tanks/ryansTank.js
@@ -3,6 +3,8 @@ class RyansTank extends BaseTank {
         super(callback, options, setup);
         this.scanInProgress = false;
         this.currentMode = 'search';
+        this.mobileDirection = 'forward';
+        this.counter = -1;
         this.directions = {
             ne: 315,
             nw: 45,
@@ -16,13 +18,37 @@ class RyansTank extends BaseTank {
     compute() {
         switch (this.currentMode) {
             case 'search':
-            this.currentMode = 'scanning';
-            this.scanVicinity();
-            this.move('stop');
-            break;
+                this.currentMode = 'scanning';
+                this.scanVicinity();
+                // this.move('stop');
+                break;
             case 'scanning':
-            this.currentMode = 'search';
-            this.move('forward');
+                this.currentMode = 'kill';
+                var boundary = this.getDistanceToBounds();
+                const { north, south, east, west } = boundary;
+                if (west > 5000 && east < 10000) {
+                    this.mobileDirection = 'backward';
+                } else if (east > 5000 && west < 4400) {
+                    this.mobileDirection = 'forward';
+                }
+                this.move(this.mobileDirection);
+                const directions = [45,135,225,315];
+                if (this.counter >= 4) {
+                    this.counter = 0;
+                } else {
+                    this.counter++;
+                }
+                this.turretTurn(directions[this.counter]);
+                console.log(boundary);
+                let targets = this.activateMagnetoDetector();
+                if (targets) {
+                    console.log("There is an enemy.");
+                }
+                console.log(targets);
+            case 'kill':
+                this.fireCannon();
+                this.currentMode = 'search';
+                break;
         }
         // console.log("Here is ryan\'s tank");
     }
